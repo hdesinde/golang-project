f32278283ba30ceb35848d3be0c1ad12faf17538
hdesinde <hugo.desinde@outlook.fr>
2020-12-24 18:51:12 +0100

    J'ai largement réécrit le code. Il est presque en état de marche, il ne manque plus grand chose

--- dijkstra.go
+++ dijkstra.go
@@ -9,12 +9,17 @@ import (
 
 //déclaration de la structure Sommet
 type Sommet struct {
-	poids float64			//Distance entre deux sommets
+	poids[] float64			//Distance entre deux sommets
 	dist float64 			//Distance du départ au sommet
 	id int					//Identifiant du sommet
 	listeVoisins []int		//Listes des sommets voisins
 	pred int				//Identifiant du sommet précédent dans le déroulé de l'algorithme 
 
+}
+
+//fonction qui permet de retirer un élément d'une liste
+func remove(slice []Sommet, s int) []Sommet {
+    return append(slice[:s], slice[(s+1):]...)
 }
 
 // un noeud est représenté par une matrice d'adjacence (id : numéro colonne/ligne)
@@ -24,20 +29,27 @@ func dijstra(graph [][]int, depart int, arrivee int) {
 
 	//Liste des sommets
 	var listeSommets = make([]Sommet, len(graph))
+
+	//initialisation des attributs de chaque sommet
 	for i := 0; i<len(graph); i++{
-		listeSommets[i].poids = math.Inf(1)					//assigne un poids infini à chaque sommet
+		listeSommets[i].dist = math.Inf(1)					//assigne une distance infinie à chaque sommet
 		listeSommets[i].id = i
-		/*if listeSommets[i].poids == float64(depart) {			//listeSommets[i].id plutôt que listeSommets[i].poids non ? Obligé de mettre float64 du coup ?
-			listeSommets[i].poids = 0							//assigne un poids nul au sommet de départ
-		}*/
+		listeSommets[i].poids = make([]float64, len(graph))	//il est nécessaire de créer toutes les cases du tableau de poids avant, car les indices seront importants
+		listeSommets[i].pred = -1
+	}
+	for i := 0; i<len(graph); i++{
 		for j := 0; j<len(graph); j++{
 			if graph[i][j] != 0{
-				listeSommets[i].listeVoisins = append (listeSommets[i].listeVoisins, j)
+				listeSommets[i].listeVoisins = append(listeSommets[i].listeVoisins, j)	//création d'un tableau de voisins
+				listeSommets[i].poids[j] = float64(graph[i][j])							//référencement du poids de chaque sommet avec ses voisins
 			}
 		}
-		fmt.Println(listeSommets[i].listeVoisins)
+		fmt.Printf("Liste des voisins de %d", i)
+		fmt.Printf(" : %v\n", listeSommets[i].listeVoisins)
+		fmt.Printf("Liste des poids de %d", i)
+		fmt.Printf(" : %v\n", listeSommets[i].poids)
 	}
-	listeSommets[depart].poids = 0
+	listeSommets[depart].dist = 0			//on assigne une distance nulle au sommet de départ 
 
 	//Liste des sommets à traiter
 	var listeSommetsATraiter = listeSommets
@@ -46,13 +58,84 @@ func dijstra(graph [][]int, depart int, arrivee int) {
 	//var listeSommetsDejaTraites = make([]Sommet, 0)
 
 	//ID sommet traité actuellement
-	var idX	int	//id point x
-	var idY	int	//id point y
+	//var idX	int	//id point x
+	//var idY	int	//id point y
 
 
 //Traitement
 
-	for {
+	//Tant que la liste des sommets à traiter n'est pas vide, faire : 	
+	for{
+		fmt.Printf("Liste des sommets à traiter : %v\n", listeSommetsATraiter)
+
+		//initialision des variables utiles 
+		min := math.Inf(1)
+		s1 := -1
+		indexListeATraiter := -1
+
+		//recherche du sommet qui possède la distance minimale, parmis les sommets encore à traiter
+		for i := 0; i<len(listeSommetsATraiter); i++{
+			if listeSommetsATraiter[i].dist < min{
+				min = listeSommetsATraiter[i].dist
+				s1 = listeSommetsATraiter[i].id
+				indexListeATraiter = i					//important de conserver l'index du sommet dans la liste listeSommetsATraiter, pour y accéder plus tard
+			}
+		}
+		fmt.Printf("min : %d\n", min)
+		fmt.Printf("s1 : %d\n", s1)
+
+		//Mise à jour des distances 
+		fmt.Println("Avant la boucle")
+		for i := 0; i<len(listeSommets[s1].listeVoisins); i++{
+			s2 := listeSommets[s1].listeVoisins[i]
+			if listeSommets[s2].dist > listeSommets[s1].dist + listeSommets[s1].poids[s2]{ 		//si la distance de début à s2 est plus grande que celle de début à s1 + celle de s1 à s2
+				listeSommets[s2].dist = listeSommets[s1].dist + listeSommets[s1].poids[s2]		//alors on prend ce nouveau chemin qui est plus court
+				listeSommets[s2].pred = s1														//et on note par où on passe
+				listeSommetsATraiter[indexListeATraiter] = listeSommets[s2]						//on actualise aussi notre liste de sommets à traiter
+				fmt.Printf("pred listeSommets : %d\n", listeSommets[s2].pred)
+				fmt.Printf("pred listeSommetsATraiter : %d\n", listeSommetsATraiter[indexListeATraiter].pred)
+			}
+		}
+		fmt.Println("Après la boucle")
+			
+		listeSommetsATraiter = remove(listeSommetsATraiter, indexListeATraiter)					//enfin, on peut supprimer le sommet sur lequel nous nous trouvons de la liste des sommets encore à traiter
+
+		if len(listeSommetsATraiter) == 0 {				//permet de sortir de la boucle lorsque la liste des sommets à traiter est vide
+			break
+		}
+	}
+
+
+//Traçage du chemin
+
+	//meilleur chemin
+	var bestWay = make([]int, len(listeSommets))
+	
+	//déclaration des variables utiles
+	s := arrivee
+	fin := 100000
+
+	//en partant de l'arrivée, nous remontons le chemin jusqu'au départ par les prédécesseurs
+	for i := 0; i<fin; i++{
+		bestWay[i] = s
+
+		if listeSommets[s].pred != -1{ 		//si un sommet n'a pas de prédécesseur, nous retournons une erreur
+			s = listeSommets[s].pred
+		}else{
+			fmt.Println("Erreur, il n'y a pas de prédécesseur disponible...")
+			break
+		}
+
+		fmt.Printf("Chemin le plus court : %v\n", bestWay)
+
+		//Lorsqu'on arrive au départ, on sort de la boucle
+		if s == depart{
+			fin = -1
+		}
+	}
+	fmt.Printf("Finalement, le chemin le plus court est : %v\n", bestWay)
+
+	/*for {
 		fmt.Println(listeSommetsATraiter)
 		for  i := 0; i<len(listeSommetsATraiter); i++ {															//on parcours chaque sommet a traiter
 			idX = listeSommetsATraiter[i].id
@@ -61,7 +144,7 @@ func dijstra(graph [][]int, depart int, arrivee int) {
 					listeSommets[listeSommetsATraiter[i].id].poids = listeSommetsATraiter[i].poids
 					//listeSommetsATraiter[listeSommetsATraiter[i].id].poids = listeSommetsATraiter[i].poids
 					//listeSommetsDejaTraites = append(listeSommetsDejaTraites, listeSommetsATraiter[i])
-					remove(listeSommetsATraiter, i)
+					listeSommetsATraiter = remove(listeSommetsATraiter, i)
 				}
 
 				for k := 0; k<len(listeSommets[idX].listeVoisins); k++{ 							//
@@ -82,9 +165,7 @@ func dijstra(graph [][]int, depart int, arrivee int) {
 		if len(listeSommetsATraiter) == 0 {				//permet de sortir de la boucle lorsque la liste des sommets à traiter est vide
 			break
 		}
-	}
-
-
+	}*/
 }
 
 func main() {
@@ -113,9 +194,5 @@ func main() {
 		}
 		fmt.Println("")
 	}
-	dijstra(grapheNoeudsOriginal, 1, 5)
-}
-
-func remove(slice []Sommet, s int) []Sommet {
-    return append(slice[:s], slice[s+1:]...)
+	dijstra(grapheNoeudsOriginal, 1, 4)
 }
